b85ed4d7a9d5793ae64b1839ea5b87a69c6295e0
sanjofumihiro <sanjofumihiro@gmail.com>
2021-04-14 18:04:03 +0000

    完成

--- realtime_YUV.py
+++ realtime_YUV.py
@@ -5,18 +5,25 @@ from matplotlib import pyplot as plt
 
 cap = cv2.VideoCapture(0)
 while True:
-    ret,frame = cap.read()
+    ret,frame_big = cap.read()
+    frame = cv2.resize(frame_big,(int (frame_big.shape[1]/4),int(frame_big.shape[0]/4)))
     YUV = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
     YUV_channel = cv2.split(YUV)
-    cv2.imshow("yuv",YUV)
-    plt.clf()
-    plt.hist(YUV_channel[0].ravel(),256,[0,256])
-    plt.hist(YUV_channel[1].ravel(),256,[0,256])
-    plt.hist(YUV_channel[2].ravel(),256,[0,256])
-    plt.pause(0.01)
-    
-    key = cv2.waitKey(100)
+    cv2.imshow("rgb",frame)
+    cv2.imshow("y",YUV_channel[0])
+    cv2.imshow("u",YUV_channel[1])
+    cv2.imshow("v",YUV_channel[2])
+    key = cv2.waitKey(1)
     if (key == ord('q')):
         break
+    if (key == ord('h')):
+        plt.clf()
+        plt.hist(YUV_channel[0].ravel(),256,[0,256])
+        plt.hist(YUV_channel[1].ravel(),256,[0,256])
+        plt.hist(YUV_channel[2].ravel(),256,[0,256])
+        plt.pause(0.001)
+    if (key == ord('c')):
+        cv2.imshow("Capture", frame_big)
+        cv2.imwrite("capture.png",frame_big)
 cap.release()
 cv2.destroyAllWindows()
